696c42a9781501bbd71452dc7bc2a20258dca318
Chris Gratigny <chrisg@ibethel.org>
2011-05-29 19:08:20 -0700

    remove prototype

--- db/migrate/20110529215240_add_sort_order_to_projects.rb
+++
@@ -1,9 +0,0 @@
-class AddSortOrderToProjects < ActiveRecord::Migration
-  def self.up
-    add_column :projects, :sort_order, :integer
-  end
-
-  def self.down
-    remove_column :projects, :sort_order
-  end
-end
